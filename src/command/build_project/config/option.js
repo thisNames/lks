a5a841c3378ddb25c75b4f79ea5d8a15e080d4bb
立秋枫林晚 <animator70@outlook.com>
2025-08-05 01:43:56 +0800

    移除废弃的项目构建命令及其相关依赖收集功能

--- src/command/build_project/config/option.js
+++
@@ -1,9 +0,0 @@
-const option = {
-    name: "lks",
-    output: "dist",
-    ignoreFiles: ["license", "LICENSE"],
-    ignoreFilesExt: [".md", ".ts"],
-    ignoresReg: []
-};
-
-module.exports = option;
